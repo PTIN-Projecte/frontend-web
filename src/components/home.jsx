701d002fe62ebed67515ebf972a8edc74415a471
CarlosSancho <8carlossanchotomas8@gmail.com>
2026-04-10 20:30:08 +0200

    Componente Login Listo para programar

---
+++ src/components/home.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function Home() {
+
+
+  return (
+    <section>
+        <header>Bienvenid@, amante de la buena comida!</header>
+    </section>
+
+  );
+}
+export default Home;
---
+++ src/components/login.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  return (
+    <
+    <section>
+        <header>LOGIN</header>
+    </section>
+
+  );
+}
+export default Login;
