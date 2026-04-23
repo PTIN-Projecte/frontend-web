a0a30dc424b9035d3131132d59975c2a7e3f36c7
CarlosSancho <8carlossanchotomas8@gmail.com>
2026-04-23 10:40:01 +0200

    Login hecho falta panel superior

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,7 +5,13 @@ function Home() {
 
   return (
     <section>
-        <header>Bienvenid@, amante de la buena comida!</header>
+      <header>Bienvenid@, amante de la buena comida!</header>
+      <nav>
+        <ul style = {{display: 'grid', gap: '10px'}}>
+          <a href="/login">Iniciar Sesión</a>
+          <a href="/register">Registrarse</a>
+        </ul>
+      </nav>
     </section>
 
   );
