c6f6f8b938238f1bf9e92a7a1a50d8f01e2ec693
asier <asier.hr15@gmail.com>
2026-05-12 17:07:57 +0200

    pruebaNFSHecha

--- src/pruebas/components/home.jsx
+++ src/pruebas/components/home.jsx
@@ -6,12 +6,20 @@ function Home() {
   return (
     <section>
       <header>Bienvenid@, amante de la buena comida!</header>
+      <div style={{ textAlign: 'center', margin: '20px 0' }}>
+      <img 
+        src="/api/images/carlitosMaricon.jpg" 
+        alt="Logo Catering" 
+        style={{ width: '200px', borderRadius: '8px' }} 
+      />
+    </div>
       <nav>
         <ul style = {{display: 'grid', gap: '10px'}}>
           <a href="/login">Iniciar Sesión</a>
           <a href="/register">Registrarse</a>
         </ul>
       </nav>
+
     </section>
 
   );
